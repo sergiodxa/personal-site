dac4650b2f72fc9985ae4fb09039c964590d3370
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-04-20 05:21:57 -0300

    project start

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('hola');
+//# sourceMappingURL=../maps/main.js.map
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var babel      = require('gulp-babel');
+var eslint     = require('gulp-eslint');
+var glob       = require('glob');
+var gulp       = require('gulp');
+var less       = require('gulp-less');
+var minifyCSS  = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
+var path       = require('path');
+var sourcemaps = require('gulp-sourcemaps');
+var uncss      = require('gulp-uncss');
+var watch      = require('gulp-watch');
+
+gulp.task('html', function () {
+  gulp
+    .src('./src/html/**/*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('css', function () {
+  gulp
+    .src('./src/less/style.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less') ],
+      filename: 'main.less',
+      ru: true
+    }))
+    .pipe(uncss({
+      html: glob.sync('./src/html/**/*.html')
+    }))
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write('../maps/'))
+    .pipe(gulp.dest('./assets/css/'))
+});
+
+gulp.task('lint', function () {
+  return gulp
+    .src('./src/es6/**/*.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
+
+gulp.task('js', ['lint'], function () {
+  gulp
+    .src('./src/es6/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write('../maps/'))
+    .pipe(gulp.dest('./assets/js/'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/html/**/*.html'], ['html']);
+  gulp.watch(['./src/less/**/*.less'], ['css']);
+  gulp.watch(['./src/es6/**/*.js'], ['js']);
+});
+
+gulp.task('default', ['html', 'css', 'js', 'watch']);
