7945350859b03cce7b7eba063101fdb00dc159c7
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-05-18 01:39:35 -0300

    repos quitados del sitio, pequeñas optimizaciones de velocidad

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
 
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){var igatajs=function(templateID,dataSet,opening,closing){try{if(typeof templateID!=='string'){return new Error('The template id must be a string.');}
-if(dataSet&&typeof dataSet!=='object'){return new Error('The dataset must be an object.');}
-if(opening&&typeof opening!=='string'){return new Error('The opening symbol must be a string.');}
-if(closing&&typeof closing!=='string'){return new Error('The closing symbol must be a string.');}
-var opening=opening||'{{';var closing=closing||'}}';function parseString(string,dataSet,opening,closing){try{var open=opening.replace(/[-[\]()*\s]/g,"\\$&").replace(/\$/g,'\\$')
-var close=closing.replace(/[-[\]()*\s]/g,"\\$&").replace(/\$/g,'\\$')
-var r=new RegExp(open+'(.+?)'+close,'g')
-var matches=string.match(r)||[];matches.forEach(function(match){var key=match.substring(opening.length,match.length-closing.length).trim();var value=typeof dataSet[key]=='undefined'?'':dataSet[key];string=string.replace(match,value);});return string;}catch(err){return err;}}
-if(templateID.indexOf('#')===-1){templateID='#'+templateID;}
-var template=document.querySelector(templateID);var clone;if(dataSet){clone=template.innerHTML.trim()
-clone=parseString(clone,dataSet,opening,closing);var parser=new DOMParser();clone=parser.parseFromString(clone,'text/html').all[2]
-clone=clone.children[0]}else{clone=document.importNode(template.content,true)}
-return clone;}catch(err){return err;}}
-module.exports=igatajs;},{}],2:[function(require,module,exports){'use strict';var _interopRequireWildcard=function(obj){return obj&&obj.__esModule?obj:{'default':obj};};var _getRepositories=require('./repositories.js');var _getRepositories2=_interopRequireWildcard(_getRepositories);window.onload=function(){_getRepositories2['default']();};},{"./repositories.js":3}],3:[function(require,module,exports){'use strict';var _interopRequireWildcard=function(obj){return obj&&obj.__esModule?obj:{'default':obj};};Object.defineProperty(exports,'__esModule',{value:true});var _template=require('igatajs');var _template2=_interopRequireWildcard(_template);var $repos=document.querySelector('#repositorios');function getRepositories(){if(!sessionStorage.repos){var request=new XMLHttpRequest();request.open('GET','https://api.github.com/users/sergiodxa/repos',true);request.onload=function(){if(this.status>=200&&this.status<400){sessionStorage.setItem('repos',this.response);var data=JSON.parse(this.response);showRepositories(data);}else{$repos.style.display='none';}};request.onerror=function onError(){$repos.style.display='none';};request.send();}else{var data=JSON.parse(sessionStorage.getItem('repos'));showRepositories(data);}}
-function showRepositories(repos){repos.forEach(function(repo){var tplt=_template2['default']('#tplt-repo',{title:repo.name,description:repo.description,link:repo.html_url});$repos.appendChild(tplt);});}
-exports['default']=getRepositories;module.exports=exports['default'];},{"igatajs":1}]},{},[2])
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){"use strict";window.onload=function(){};},{}]},{},[1])
 //# sourceMappingURL=../maps/main.js.map
