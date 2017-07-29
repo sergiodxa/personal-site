ba521ad0bd9972a33031325608dfb128c807ca9d
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-07-29 03:12:01 -0500

    Site component

---
+++ components/site.js
@@ -0,0 +1,35 @@
+import { format } from 'url'
+import parseUrl from '../lib/parse-url.js'
+
+export default ({ link, isSelf }) =>
+  <small className="site">
+    <a
+      className="site-url"
+      href={format({ hostname: parseUrl(link).hostname, protocol: parseUrl(link).protocol })}
+      target="_blank"
+      rel="nofollow"
+    >
+      [{isSelf ? 'self' : parseUrl(link).hostname}]
+    </a>
+    <style jsx>{`
+      .site {
+        display: none;
+      }
+
+      .site-url {
+        flex: 1;
+        text-decoration: none;
+        color: rgba(0, 0, 0, .7);
+      }
+
+      .site-url:hover {
+        text-decoration: underline;
+      }
+
+      @media (min-width: 720px) {
+        .site {
+          display: inline;
+        }
+      }
+    `}</style>
+  </small>
