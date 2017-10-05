21d7753e70c562c074baba6d2d589b7a1842addc
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-10-05 14:12:31 -0500

    Use withError and compose HOC

--- pages/essays/index.js
+++ pages/essays/index.js
@@ -7,10 +7,13 @@ import Layout from '../../layouts/main.js';
 
 import essays from '../../data/essays.js';
 
+import compose from '../../lib/compose.js';
+
+import withError from '../../lib/with-error.js';
 import withGA from '../../lib/with-ga.js';
 import withSW from '../../lib/with-sw.js';
 
-const sort = (a, b) => {
+const byDate = (a, b) => {
   const timeA = new Date(a.date).getTime();
   const timeB = new Date(b.date).getTime();
 
@@ -18,44 +21,42 @@ const sort = (a, b) => {
   if (timeA < timeB) return 1;
   return 0;
 };
-const map = essay => <Essay key={essay.link} {...essay} />;
+const toComponent = essay => <Essay key={essay.link} {...essay} />;
 
-export default withGA(
-  withSW(() => (
-    <Layout>
-      <Head>
-        <title>Essays and Articles by Sergio Xalambrí</title>
-      </Head>
+export default compose(withError, withGA, withSW)(() => (
+  <Layout>
+    <Head>
+      <title>Essays and Articles by Sergio Xalambrí</title>
+    </Head>
 
-      <Link href="/" prefetch>
-        <a title="Sergio Xalambrí">
-          <Header />
-        </a>
-      </Link>
+    <Link href="/" prefetch>
+      <a title="Sergio Xalambrí">
+        <Header />
+      </a>
+    </Link>
 
-      <section>{essays.sort(sort).map(map)}</section>
+    <section>{essays.sort(byDate).map(toComponent)}</section>
 
-      <style jsx global>{`
-        main {
-          margin-bottom: 2em;
-        }
-      `}</style>
-      <style jsx>{`
-        a {
-          color: black;
-          text-decoration: none;
-        }
+    <style jsx global>{`
+      main {
+        margin-bottom: 2em;
+      }
+    `}</style>
+    <style jsx>{`
+      a {
+        color: black;
+        text-decoration: none;
+      }
 
+      section {
+        padding: 0.5em;
+      }
+
+      @media (min-width: 720px) {
         section {
-          padding: 0.5em;
+          padding: 1em;
         }
-
-        @media (min-width: 720px) {
-          section {
-            padding: 1em;
-          }
-        }
-      `}</style>
-    </Layout>
-  ))
-);
+      }
+    `}</style>
+  </Layout>
+));
