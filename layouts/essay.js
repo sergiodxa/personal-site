a2db49470369a1739907f456d0cc9675b5d5ee4d
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-10-07 13:11:25 -0500

    Add more styles to essays content

--- layouts/essay.js
+++ layouts/essay.js
@@ -51,34 +51,40 @@ export default ({ title, content, date, slug, description }) => (
         text-decoration: none;
       }
 
-      section {
+      .content {
         margin-bottom: 10vh;
       }
 
-      section :global(article) {
+      .content :global(*:not(iframe):empty) {
+        display: none;
+      }
+
+      .content :global(article) {
         outline: 0;
         word-break: break-word;
         word-wrap: break-word;
         max-width: 740px;
-        padding-left: 20px;
-        padding-right: 20px;
+        padding-left: 1.25rem;
+        padding-right: 1.25rem;
         width: 100%;
         margin: 0 auto;
         box-sizing: border-box;
       }
-
-      section :global(h2),
-      section :global(h3) {
+      .content :global(h2),
+      .content :global(h3),
+      .content :global(h4),
+      .content :global(h5),
+      .content :global(h6) {
         font-weight: bold;
       }
 
-      section :global(h2) {
+      .content :global(h2) {
         font-size: 2.5em;
         letter-spacing: -0.028em;
         margin: 0;
       }
 
-      section :global(h3) {
+      .content :global(h3) {
         font-size: 2em;
         letter-spacing: -0.02em;
         margin: 0;
@@ -86,14 +92,15 @@ export default ({ title, content, date, slug, description }) => (
         margin-top: 3.5rem;
       }
 
-      section :global(a) {
+      .content :global(a) {
         color: black;
       }
 
-      section :global(blockquote) {
+      .content :global(blockquote) {
         border-left: 3px solid rgba(0, 0, 0, 0.84);
-        padding-left: 20px;
-        margin-left: -23px;
+        box-sizing: border-box;
+        padding-left: calc(2rem - 3px);
+        margin-left: -2rem;
         padding-bottom: 2px;
         letter-spacing: 0.01rem;
         font-weight: 400;
@@ -103,13 +110,13 @@ export default ({ title, content, date, slug, description }) => (
         letter-spacing: -0.003em;
       }
 
-      section :global(blockquote > p) {
+      .content :global(blockquote > p) {
         margin: 0;
       }
 
-      section :global(p) {
+      .content :global(p) {
         font-weight: lighter;
-        margin: 1em 0;
+        margin: 1rem 0;
         letter-spacing: 0.01rem;
         font-weight: 400;
         font-style: normal;
@@ -118,20 +125,213 @@ export default ({ title, content, date, slug, description }) => (
         letter-spacing: -0.003em;
       }
 
+      /* inline code */
+      .content :global(p code) {
+        color: #f81ce5;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace,
+          serif;
+        white-space: pre-wrap;
+      }
+
+      .content :global(p code:before) {
+        content: '\`';
+      }
+
+      .content :global(p code:after) {
+        content: '\`';
+      }
+
+      /* code blocks */
+      .content :global(pre) {
+        border: 1px solid black;
+        padding: 1rem calc(2rem - 1px);
+        margin: 1rem -2rem;
+        overflow: scroll;
+      }
+
+      .content :global(pre code) {
+        color: black;
+        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace,
+          serif;
+        font-size: 1rem;
+      }
+
+      .content :global(pre code[class="language-javascript"]),
+      .content :global(pre code[class="language-js"]),
+      .content :global(pre code[class="language-html"]),
+      .content :global(pre code[class="language-css"]),
+      .content :global(pre code[class="language-styl"]),
+      .content :global(pre code[class="language-saas"]),
+      .content :global(pre code[class="language-less"]),
+      .content :global(pre code[class="language-ruby"] ){
+        tab-size: 2;
+      }
+      .content :global(pre code[class="language-java"]),
+      .content :global(pre code[class="language-python"]),
+      .content :global(pre code[class="language-php"]) {
+        tab-size: 4;
+      }
+      .content :global(pre code[class="language-go"]) {
+        tab-size: 8;
+      }
+
+      .content :global(table) {
+        font-size: 0.9rem;
+        width: 100%;
+      }
+
+      .content :global(th) {
+        padding: 5px;
+        color: #9b9b9b;
+        font-style: oblique;
+        font-weight: normal;
+        text-align: left;
+      }
+
+      .content :global(td) {
+        padding: 5px;
+      }
+
+      .content :global(mark) {
+        background-color: #f81ce5;
+        color: white;
+        padding: 0.125em 0.2em;
+      }
+
+      .content :global(ul),
+      .content :global(ol) {
+        margin-left: -2rem;
+        padding-left: 2rem;
+      }
+
+      .content :global(ul) {
+        list-style-type: square;
+      }
+
+      .content :global(ol) {
+        list-style-type: lower-roman;
+      }
+
+      .content :global(li) {
+        line-height: 1.5;
+      }
+
+      .content :global(abbr) {
+        cursor: help;
+        text-decoration-style: dashed;
+      }
+
+      .content :global(figure) {
+        text-align: center;
+        width: 100%;
+      }
+
+      .content :global(img) {
+        max-width: 100%;
+      }
+
+      .content :global(dt) {
+        font-weight: bold;
+      }
+
+      .content :global(.embed-responsive) {
+        position: relative;
+        padding-bottom: 56.25% /* 16:9 */;
+        height: 0;
+        margin: 0 -2rem;
+      }
+
+      .content :global(.embed-responsive-item) {
+        position: absolute !important;
+        top: 0 !important;
+        left: 0 !important;
+        width: 100% !important;
+        height: 100% !important;
+      }
+
+      /* pink */
+      .content :global(.hljs-keyword),
+      .content :global(.hljs-selector-tag),
+      .content :global(.hljs-literal),
+      .content :global(.hljs-strong),
+      .content :global(.hljs-attribute),
+      .content :global(.hljs-symbol),
+      .content :global(.hljs-link) {
+        color: #f81ce5;
+      }
+      /* red */
+      .content :global(.hljs-code),
+      .content :global(.hljs-deletion),
+      .content :global(.hljs-name) {
+        color: #ff5f56;
+      }
+      /* green */
+      .content :global(.hljs-bullet),
+      .content :global(.hljs-subst),
+      .content :global(.hljs-title),
+      .content :global(.hljs-section),
+      .content :global(.hljs-emphasis),
+      .content :global(.hljs-type),
+      .content :global(.hljs-built_in),
+      .content :global(.hljs-builtin-name),
+      .content :global(.hljs-selector-attr),
+      .content :global(.hljs-selector-pseudo),
+      .content :global(.hljs-addition),
+      .content :global(.hljs-variable),
+      .content :global(.hljs-template-tag),
+      .content :global(.hljs-template-variable),
+      .content :global(.hljs-function),
+      .content :global(.hljs-attr) {
+        color: #27c93f;
+      }
+      /* yellow */
+      .content :global(.hljs-string),
+      .content :global(.hljs-regexp) {
+        color: #FFBD2F;
+      }
+      /* cyan */
+      .content :global(.hljs-attribute) {
+        color: #067df7;
+      }
+      /* grey */
+      .content :global(.hljs-comment),
+      .content :global(.hljs-quote),
+      .content :global(.hljs-meta) {
+        color: #9b9b9b;
+      }
+      /* black */
+      .content :global(.hljs-params),
+      .content :global(.hljs-tag) {
+        color: black;
+      }
+      /* bold */
+      .content :global(.hljs-keyword),
+      .content :global(.hljs-selector-tag),
+      .content :global(.hljs-literal),
+      .content :global(.hljs-doctag),
+      .content :global(.hljs-title),
+      .content :global(.hljs-section),
+      .content :global(.hljs-type),
+      .content :global(.hljs-selector-id) {
+        font-weight: bold;
+      }
+
       @media (max-width: 767px) {
-        section :global(h2) {
+        .content :global(h2) {
           font-size: 2rem;
           margin-left: -2px;
           line-height: 1.04;
           letter-spacing: -0.028em;
         }
 
-        section :global(h3) {
+        .content :global(h3) {
           font-size: 1.75rem;
           margin-top: 1.75rem;
         }
 
-        section :global(blockquote) {
+        .content :global(blockquote) {
           font-size: 1.125rem;
           line-height: 1.58;
           letter-spacing: -0.004em;
@@ -139,10 +339,27 @@ export default ({ title, content, date, slug, description }) => (
           margin-left: -20px;
         }
 
-        section :global(p) {
+        .content :global(p) {
           font-size: 1.125rem;
           line-height: 1.58;
           letter-spacing: -0.004em;
+        }
+
+        .content :global(pre) {
+          border-left: none;
+          border-right: none;
+          padding: 1rem 1.25rem;
+          margin: 1rem -1.25rem;
+        }
+
+        .content :global(ul),
+        .content :global(ol) {
+          margin-left: 0;
+          padding-left: 2rem;
+        }
+
+        .content :global(.embed-responsive) {
+          margin: 0 -1.25rem;
         }
       }
     `}</style>
