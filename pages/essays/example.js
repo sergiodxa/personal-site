03f7e514a5b8827cf20ee5268300364c12ba1c40
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-10-13 21:06:43 -0500

    Example essay to check styles (only in dev mode)

---
+++ pages/essays/example.js
@@ -0,0 +1,54 @@
+import compose from 'recompose/compose';
+
+import Essay from '../../layouts/essay.js';
+
+import withError from '../../lib/with-error.js';
+import withGA from '../../lib/with-ga.js';
+import withSW from '../../lib/with-sw.js';
+
+export default compose(withError, withGA, withSW)(() => (
+  <Essay
+    title="This is an example"
+    date="2017-09-30T00:00:00Z"
+    description=""
+    content={`
+*[HTML]: HyperText Markup Language
+
+# This is an example
+This is [used](https://zeit.co) to \`display\` **every** _possible_ styled HTML ++element++ ==parsed from Markdown==.
+
+# Heading 1
+## Heading 2
+### Heading 3
+#### Heading 4
+##### Heading 5
+###### Heading 6
+
+> This is a quote
+> by _Sergio Xalambrí_
+
+\`\`\`diff
++ New code
+- Old code
+\`\`\`
+
+\`\`\`js
+export default () => (
+  <h1>Heading 1</h1>
+)
+\`\`\`
+
+- this
+- is
+- a
+- list
+
+1. Item 1
+1. Item 2
+1. Item 3
+1. Item 4
+
+![This is my photo](/static/now-black.png)
+    `}
+  />
+));
