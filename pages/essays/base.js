3267d40c88fbf9c90f5be827c5459c6170202c3f
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-10-16 10:52:28 -0500

    Create base empty essay ready to be copied

---
+++ pages/essays/base.js
@@ -0,0 +1,18 @@
+import compose from 'recompose/compose';
+
+import Essay from '../../layouts/essay.js';
+
+import withError from '../../lib/with-error.js';
+import withGA from '../../lib/with-ga.js';
+import withSW from '../../lib/with-sw.js';
+
+export default compose(withError, withGA, withSW)(() => (
+  <Essay
+    title="Title"
+    date="YYYY-MM-DDT00:00:00Z"
+    description=""
+    content={`
+# Title
+    `}
+  />
+));
