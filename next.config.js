0b38c2f4be2cbb2be3ff48ee29ae0914ac114ce3
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-10-18 01:33:13 -0500

    Only create SW in prod

--- next.config.js
+++ next.config.js
@@ -3,27 +3,26 @@ const BabiliPlugin = require('babili-webpack-plugin');
 
 module.exports = {
   webpack(config, { dev }) {
-    config.plugins.push(
-      new SWPrecacheWebpackPlugin({
-        verbose: true,
-        staticFileGlobsIgnorePatterns: [/\.next\//],
-        minify: !dev,
-        cacheId: 'sergio.now.sh',
-        runtimeCaching: [
-          {
-            handler: 'networkFirst',
-            urlPattern: /^https?.*/
-          }
-        ]
-      })
-    );
-
     config.plugins = config.plugins.filter(plugin => {
       return plugin.constructor.name !== 'UglifyJsPlugin';
     });
 
     if (!dev) {
-      config.plugins.push(new BabiliPlugin());
+      config.plugins.push(
+        new SWPrecacheWebpackPlugin({
+          verbose: true,
+          staticFileGlobsIgnorePatterns: [/\.next\//],
+          minify: !dev,
+          cacheId: 'sergio.now.sh',
+          runtimeCaching: [
+            {
+              handler: 'networkFirst',
+              urlPattern: /^https?.*/
+            }
+          ]
+        }),
+        new BabiliPlugin()
+      );
     }
 
     return config;
