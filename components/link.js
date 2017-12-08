2c39c42a62e37d78f9d9d139764257c2fe62ff46
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-12-08 02:33:09 -0500

    Fix dev mode propTypes warning

--- components/link.js
+++ components/link.js
@@ -1,13 +1,31 @@
-import { bool } from 'prop-types';
+import PropTypes from 'prop-types';
 import Link from 'next/link';
 import Router from 'next/router';
 import { format, resolve, parse } from 'url';
 
 export default class LinkWithData extends Link {
-  static propTypes = {
-    ...Link.propTypes,
-    withData: bool
-  };
+  static propTypes = exact({
+    href: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
+    as: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+    prefetch: PropTypes.bool,
+    replace: PropTypes.bool,
+    shallow: PropTypes.bool,
+    passHref: PropTypes.bool,
+    scroll: PropTypes.bool,
+    withData: PropTypes.bool
+    children: PropTypes.oneOfType([
+      PropTypes.element,
+      (props, propName) => {
+        const value = props[propName]
+
+        if (typeof value === 'string') {
+          warnLink(`Warning: You're using a string directly inside <Link>. This usage has been deprecated. Please add an <a> tag as child of <Link>`)
+        }
+
+        return null
+      }
+    ]).isRequired
+  });
 
   async prefetch() {
     if (!this.props.prefetch) return;
