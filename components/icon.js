482493674e27a8a493fe98ed8a5ae874241fa4e1
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-12-15 04:18:19 -0500

    Load icons as React components with SVG

--- components/icon.js
+++ components/icon.js
@@ -1,9 +1,51 @@
 import Head from 'next/head';
 
-export default ({ name }) => (
-  <i className={`icon-${name.toLowerCase()}`} alt={name}>
-    <Head>
-      <link rel="stylesheet" href="/static/icons/style.css" />
-    </Head>
-  </i>
-);
+import Email from './icons/email';
+import Essays from './icons/essays';
+import GitHub from './icons/github';
+import Instagram from './icons/instagram';
+import LinkedIn from './icons/linkedin';
+import Medium from './icons/medium';
+import Meetup from './icons/meetup';
+import NPM from './icons/npm';
+import Spotify from './icons/spotify';
+import Steam from './icons/steam';
+import Twitter from './icons/twitter';
+import YouTube from './icons/youtube';
+
+export default ({ name }) => {
+  return (
+    <i alt={name}>
+      {(() => {
+        switch (name.toLowerCase()) {
+          case 'essays':
+            return <Essays />;
+          case 'email':
+            return <Email />;
+          case 'essay':
+            return <Essay />;
+          case 'github':
+            return <GitHub />;
+          case 'instagram':
+            return <Instagram />;
+          case 'linkedin':
+            return <LinkedIn />;
+          case 'medium':
+            return <Medium />;
+          case 'meetup':
+            return <Meetup />;
+          case 'npm':
+            return <NPM />;
+          case 'spotify':
+            return <Spotify />;
+          case 'steam':
+            return <Steam />;
+          case 'twitter':
+            return <Twitter />;
+          case 'youtube':
+            return <YouTube />;
+        }
+      })()}
+    </i>
+  );
+};
---
+++ components/icons/essays.js
@@ -0,0 +1,12 @@
+export default () => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    width={32}
+    height={32}
+    viewBox="0 0 32 32"
+  >
+    <title>essays</title>
+    <path d="M32 4h-32v-4h32zM20 12h-20v-4h20zM32 24h-32v-4h32zM24 32h-24v-4h24zM32 30c0 1.105-0.897 2-1.998 2-1.108 0-2.002-0.895-2.002-2s0.894-2 2.002-2c1.101 0 1.998 0.895 1.998 2z" />
+  </svg>
+);
