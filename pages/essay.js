ad82e12416f10891e570c6e13fdd82a4d2cbae01
sergiodxa <sergiodxa@gmail.com>
2017-12-25 22:43:11 -0500

    Remove console.group

--- pages/essay.js
+++ pages/essay.js
@@ -17,62 +17,57 @@ import withSW from '../lib/with-sw.js';
 const gql = String.raw;
 
 async function getInitialProps(ctx) {
-  console.group('getInitialProps');
-  try {
-    const url = ctx.asPath || format({ pathname: ctx.pathname });
-    console.debug('URL:', url);
-  
-    const isServer = Boolean(ctx.req);
-    console.debug('Is server?', isServer);
-  
-    if (!isServer) {
-      console.info('Getting info from localStorage');
-      const props = localStorage.getItem(url);
-      console.debug('Retrieved info:', props);
+  const url = ctx.asPath || format({ pathname: ctx.pathname });
+  console.debug('URL:', url);
 
-      if (props) {
-        return JSON.parse(props);
-      }
+  const isServer = Boolean(ctx.req);
+  console.debug('Is server?', isServer);
+
+  if (!isServer) {
+    console.info('Getting info from localStorage');
+    const props = localStorage.getItem(url);
+    console.debug('Retrieved info:', props);
+
+    if (props) {
+      return JSON.parse(props);
     }
-  
-    const query = gql`
-      query getEssay($slug: String!) {
-        getEssay(slug: $slug) {
-          title
-          slug
-          content
-          date
-          description
-          canonicalUrl
-        }
-      }
-    `;
-  
-    const variables = {
-      slug: ctx.query.slug
-    };
-  
-    console.info('Fetching data from GraphQL API');
-    console.debug('Variables:', JSON.stringify(variables, null, 2));
-    const { data, errors } = await fetch({ query, variables });
-  
-    if (errors) {
-      console.debug('Errors:', JSON.stringify(errors, null, 2));
-      return { errors };
-    }
-    
-    const props = { ...data.getEssay };
-    console.debug('Props:', JSON.stringify(props, null, 2));
-  
-    if (ctx.isVirtualCall) {
-      console.info('Is a virtual call');
-      localStorage.setItem(url, JSON.stringify(props));
-    }
-  
-    return props;
-  } finally {
-    console.groupEnd('getInitialProps');
   }
+
+  const query = gql`
+    query getEssay($slug: String!) {
+      getEssay(slug: $slug) {
+        title
+        slug
+        content
+        date
+        description
+        canonicalUrl
+      }
+    }
+  `;
+
+  const variables = {
+    slug: ctx.query.slug
+  };
+
+  console.info('Fetching data from GraphQL API');
+  console.debug('Variables:', JSON.stringify(variables, null, 2));
+  const { data, errors } = await fetch({ query, variables });
+
+  if (errors) {
+    console.debug('Errors:', JSON.stringify(errors, null, 2));
+    return { errors };
+  }
+
+  const props = { ...data.getEssay };
+  console.debug('Props:', JSON.stringify(props, null, 2));
+
+  if (ctx.isVirtualCall) {
+    console.info('Is a virtual call');
+    localStorage.setItem(url, JSON.stringify(props));
+  }
+
+  return props;
 }
 
 export default compose(
