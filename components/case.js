fc69e473fbc4c6b2e3aa5d7102c2e88b9d1599de
sergiodxa <sergiodxa@gmail.com>
2018-01-07 01:05:11 -0500

    Improve Case component code

--- components/case.js
+++ components/case.js
@@ -1,6 +1,8 @@
 function Case({ value, ...cases }) {
-  const component =
-    cases[`on${value.slice(0, 1).toUpperCase()}${value.slice(1)}`];
+  const caseName = `on${value.slice(0, 1).toUpperCase()}${value
+    .slice(1)
+    .toLowerCase()}`;
+  const component = cases[caseName];
   if (!component) return null;
   return component;
 }
