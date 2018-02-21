0055a7067a8a880c074b422bcaa59f1a20d88faf
sergiodxa <sergiodxa@gmail.com>
2018-02-20 21:14:25 -0500

    Simplify essay page to export definition

--- server/routes.js
+++ server/routes.js
@@ -1,84 +1,26 @@
+const essay = slug => ({
+  [`/essays/${slug}`]: {
+    page: "/essay",
+    query: { slug }
+  }
+});
+
 module.exports = {
   "/": { page: "/" },
   "/about": { page: "/about" },
   "/subscribe": { page: "/subscribe" },
   "/essays": { page: "/essays" },
-  "/essays/an-accessible-approach-to-frontend-testing": {
-    page: "/essay",
-    query: {
-      slug: "an-accessible-approach-to-frontend-testing"
-    }
-  },
-  "/essays/how-to-keep-updated-with-the-javascript-ecosystem": {
-    page: "/essay",
-    query: {
-      slug: "how-to-keep-updated-with-the-javascript-ecosystem"
-    }
-  },
-  "/essays/bye-platzi-hi-zeit": {
-    page: "/essay",
-    query: {
-      slug: "bye-platzi-hi-zeit"
-    }
-  },
-  "/essays/sobre-el-ecosistema-y-la-fatiga-de-javascript": {
-    page: "/essay",
-    query: {
-      slug: "sobre-el-ecosistema-y-la-fatiga-de-javascript"
-    }
-  },
-  "/essays/documentation": {
-    page: "/essay",
-    query: {
-      slug: "documentation"
-    }
-  },
-  "/essays/markdown-react": {
-    page: "/essay",
-    query: {
-      slug: "markdown-react"
-    }
-  },
-  "/essays/definiendo-conceptos-closures-y-scope": {
-    page: "/essay",
-    query: {
-      slug: "definiendo-conceptos-closures-y-scope"
-    }
-  },
-  "/essays/combinando-react-y-redux": {
-    page: "/essay",
-    query: {
-      slug: "combinando-react-y-redux"
-    }
-  },
-  "/essays/introduccion-a-redux": {
-    page: "/essay",
-    query: {
-      slug: "introduccion-a-redux"
-    }
-  },
-  "/essays/como-mantenerse-actualizado-con-el-ecosistema-de-javascript": {
-    page: "/essay",
-    query: {
-      slug: "como-mantenerse-actualizado-con-el-ecosistema-de-javascript"
-    }
-  },
-  "/essays/documentation": {
-    page: "/essay",
-    query: {
-      slug: "documentation"
-    }
-  },
-  "/essays/documentacion": {
-    page: "/essay",
-    query: {
-      slug: "documentacion"
-    }
-  },
-  "/essays/introduccion-a-graphql": {
-    page: "/essay",
-    query: {
-      slug: "introduccion-a-graphql"
-    }
-  }
+  ...essay("an-accessible-approach-to-frontend-testing"),
+  ...essay("how-to-keep-updated-with-the-javascript-ecosystem"),
+  ...essay("bye-platzi-hi-zeit"),
+  ...essay("sobre-el-ecosistema-y-la-fatiga-de-javascript"),
+  ...essay("documentation"),
+  ...essay("markdown-react"),
+  ...essay("definiendo-conceptos-closures-y-scope"),
+  ...essay("combinando-react-y-redux"),
+  ...essay("introduccion-a-redux"),
+  ...essay("como-mantenerse-actualizado-con-el-ecosistema-de-javascript"),
+  ...essay("documentation"),
+  ...essay("documentacion"),
+  ...essay("introduccion-a-graphql"),
 };
