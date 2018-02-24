5afa13400ddf7728afe57eb42c47747b9cb38596
sergiodxa <sergiodxa@gmail.com>
2018-02-24 18:15:43 -0500

    pkg the site and deploy it

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const next = require("next");
+const { join } = require("path");
 const { parse } = require("url");
+const LRUCache = require("lru-cache");
 
 const dev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 3001;
@@ -10,14 +12,68 @@ const handle = app.getRequestHandler();
 
 const routes = require("./routes");
 
+const ssrCache = new LRUCache({
+  max: 100,
+  maxAge: 1000 * 60 * 60 // 1hour
+});
+
+function getCacheKey(req) {
+  return `${req.url}`;
+}
+
+async function renderAndCache(req, res, pagePath, queryParams) {
+  const key = getCacheKey(req);
+
+  if (ssrCache.has(key)) {
+    res.setHeader("X-Cache", "HIT");
+    res.send(ssrCache.get(key));
+    return;
+  }
+
+  try {
+    const html = await app.renderToHTML(req, res, pagePath, queryParams);
+
+    if (res.statusCode !== 200) {
+      res.send(html);
+      return;
+    }
+
+    ssrCache.set(key, html);
+
+    res.setHeader("X-Cache", "MISS");
+    res.send(html);
+  } catch (err) {
+    app.renderError(err, req, res, pagePath, queryParams);
+  }
+}
+
 app.prepare().then(() => {
   const server = express();
+
+  server.get("/service-worker.js", (req, res) => {
+    const { pathname } = parse(req.url);
+    const filePath = join(__dirname, "..", ".next", pathname);
+    app.serveStatic(req, res, filePath);
+  });
+
+  server.get("/atom", (req, res) => {
+    const { pathname } = parse(req.url);
+    const filePath = join(__dirname, "..", ".next", pathname);
+    app.serveStatic(req, res, filePath);
+  });
+
+  server.get("/_next/**", (req, res) => {
+    res.append("Cache-Control", "max-age=31536000");
+    res.append("Cache-Control", "immutable");
+    return handle(req, res);
+  });
+
   server.get("*", (req, res) => {
     const parsedUrl = parse(req.url, true);
     const { pathname, query = {} } = parsedUrl;
     const route = routes[pathname];
     if (route) {
-      return app.render(req, res, route.page, route.query);
+      return renderAndCache(req, res, route.page, route.query);
     }
     return handle(req, res);
   });
--- server/routes.js
+++ server/routes.js
@@ -5,29 +5,32 @@ const essay = slug => ({
   }
 });
 
-module.exports = {
-  "/": { page: "/" },
-  "/about": { page: "/about" },
-  "/subscribe": { page: "/subscribe" },
-  "/contact": { page: "/contact" },
-  "/services": { page: "/services" },
-  "/uses": { page: "/uses" },
-  "/essays": { page: "/essays" },
-  ...essay("an-accessible-approach-to-frontend-testing"),
-  ...essay("how-to-keep-updated-with-the-javascript-ecosystem"),
-  ...essay("bye-platzi-hi-zeit"),
-  ...essay("sobre-el-ecosistema-y-la-fatiga-de-javascript"),
-  ...essay("documentation"),
-  ...essay("markdown-react"),
-  ...essay("definiendo-conceptos-closures-y-scope"),
-  ...essay("combinando-react-y-redux"),
-  ...essay("introduccion-a-redux"),
-  ...essay("como-mantenerse-actualizado-con-el-ecosistema-de-javascript"),
-  ...essay("documentation"),
-  ...essay("documentacion"),
-  ...essay("introduccion-a-graphql"),
-  ...essay("ciclo-de-vida-de-un-componente-de-reactjs"),
-  ...essay("que-son-y-como-funcionan-las-promesas-en-javascript"),
-  ...essay("mezclando-flujos-sincronos-y-asincronos"),
-  ...essay("implementando-un-servidor-de-graphql")
-};
+module.exports = Object.assign(
+  {
+    "/": { page: "/" },
+    "/about": { page: "/about" },
+    "/subscribe": { page: "/subscribe" },
+    "/contact": { page: "/contact" },
+    "/services": { page: "/services" },
+    "/uses": { page: "/uses" },
+    "/essays": { page: "/essays" }
+  },
+  essay("an-accessible-approach-to-frontend-testing"),
+  essay("an-accessible-approach-to-frontend-testing"),
+  essay("how-to-keep-updated-with-the-javascript-ecosystem"),
+  essay("bye-platzi-hi-zeit"),
+  essay("sobre-el-ecosistema-y-la-fatiga-de-javascript"),
+  essay("documentation"),
+  essay("markdown-react"),
+  essay("definiendo-conceptos-closures-y-scope"),
+  essay("combinando-react-y-redux"),
+  essay("introduccion-a-redux"),
+  essay("como-mantenerse-actualizado-con-el-ecosistema-de-javascript"),
+  essay("documentation"),
+  essay("documentacion"),
+  essay("introduccion-a-graphql"),
+  essay("ciclo-de-vida-de-un-componente-de-reactjs"),
+  essay("que-son-y-como-funcionan-las-promesas-en-javascript"),
+  essay("mezclando-flujos-sincronos-y-asincronos"),
+  essay("implementando-un-servidor-de-graphql")
+);
