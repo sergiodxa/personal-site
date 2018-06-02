32c990bc98b99fac83db34a8d5909639b01d4d17
Sergio Xalambrí <sergiodxa@gmail.com>
2018-06-01 23:49:07 -0500

    Add morer cases for tests

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,6 +1,21 @@
 describe("Homepage", () => {
-  it("should render my name", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("should render my name", () => {
     cy.contains("Sergio Xalambrí");
+  });
+
+  it("should render my job title", () => {
+    cy.contains("Lead Support Engineer at ▲ ZEIT");
+  });
+  
+  it("should render the navigation", () => {
+    cy.contains("About me");
+    cy.contains("Essays");
+    cy.contains("Services");
+    cy.contains("Thoughts");
+    cy.contains("Books");
   });
 });
