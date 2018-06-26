931abd6ec7cb49e6e485b421378589f30b8f9f08
Sergio Xalambrí <sergiodxa@gmail.com>
2018-06-26 14:04:01 -0500

    Remove Twitter widget script

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,12 @@
 import Document, { Head, Main, NextScript } from "next/document";
 
-export default class extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
       <html lang="en">
         <Head />
         <body>
           <Main />
-          <script async src="//platform.twitter.com/widgets.js" />
           <NextScript />
         </body>
       </html>
