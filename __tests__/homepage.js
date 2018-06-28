35d28ebd9dccda1b3ea7868021b44d3da3a07cab
Sergio Xalambrí <sergiodxa@gmail.com>
2018-06-28 11:26:31 -0500

    Improve speed of testing running page.goto only one time

--- __tests__/homepage.js
+++ __tests__/homepage.js
@@ -1,13 +1,13 @@
 describe("Home Page", () => {
-  it("should render my name", async () => {
-    await page.goto("http://localhost:3000");
+  beforeAll(async () => {
+    await page.goto("http://localhost:3000/");
+  });
 
+  it("should render my name", async () => {
     await expect(page).toMatch("Sergio Xalambrí");
   });
 
   it("should render my job title", async () => {
-    await page.goto("http://localhost:3000");
-
     await expect(page).toMatch("Lead Support Engineer");
   });
 });
