8067843d1fc6b4f9499e4fb320b9c3ad0771815e
Sergio Xalambrí <sergiodxa@gmail.com>
2018-06-28 10:53:15 -0500

    Simplify tests

--- __tests__/homepage.js
+++ __tests__/homepage.js
@@ -1,23 +1,13 @@
-const puppeteer = require("puppeteer");
-
 describe("Home Page", () => {
   it("should render my name", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
     await page.goto("http://localhost:3000");
 
     await expect(page).toMatch("Sergio Xalambrí");
-
-    return browser.close();
   });
 
   it("should render my job title", async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
     await page.goto("http://localhost:3000");
 
     await expect(page).toMatch("Lead Support Engineer");
-
-    return browser.close();
   });
 });
