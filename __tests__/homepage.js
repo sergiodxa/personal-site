04b076a94dc32f39b6e6d7fce195aed415f77b19
Sergio Xalambrí <hello@sergiodxa.com>
2018-09-12 01:12:27 -0500

    Fix tests

--- __tests__/homepage.js
+++ __tests__/homepage.js
@@ -1,13 +1,19 @@
 describe("Page /", () => {
   beforeAll(async () => {
-    await page.goto("http://localhost:3000/");
+    await page.goto("http://localhost:3000");
   });
 
   it("should render my name", async () => {
     await expect(page).toMatch("Sergio Xalambrí");
   });
 
-  it("should render my job title", async () => {
-    await expect(page).toMatch("Software Engineer");
+  it("should render my title", async () => {
+    await expect(page).toMatch("Software Engineer & Technical Writer");
+  });
+
+  it("should render the buy me a coffee suggestion", async () => {
+    await expect(page).toMatch(
+      "Do you like my content? Become a Patreon and get some benefits!"
+    );
   });
 });
