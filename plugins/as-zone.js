be1c988a365eb15b20cfa109271ec057172dc3c2
Sergio Xalambrí <sergio@able.co>
2018-10-29 15:14:49 -0500

    Don't set default asset prefix if is not prod

--- plugins/as-zone.js
+++ plugins/as-zone.js
@@ -3,7 +3,8 @@ const { NODE_ENV = "development" } = process.env;
 const isProd = NODE_ENV === "production";
 
 module.exports = alias => (nextConfig = {}) => {
+  if (!isProd) return nextConfig;
   return Object.assign({}, nextConfig, {
-    assetPrefix: isProd ? alias : "http://localhost:3000"
+    assetPrefix: alias
   });
 };
