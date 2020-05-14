7bac88c532012a158c473cec7709f96b7d17ea9b
Sergio Xalambrí <hello@sergiodxa.com>
2020-05-14 03:33:16 -0500

    Update tag styles

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -74,7 +74,7 @@ function SectionContent({ children }: { children: React.ReactNode }) {
 
 function Tag({ children }: { children: React.ReactNode }) {
   return (
-    <small className="bg-yellow-500 hover:bg-black text-black hover:text-yellow-500 py-1 px-3 mr-2 my-1 leading-none text-xs">
+    <small className="bg-yellow-500 hover:bg-black text-black hover:text-yellow-500 py-1 px-3 mr-2 my-1 leading-none text-xs rounded select-all cursor-default">
       {children}
     </small>
   );
