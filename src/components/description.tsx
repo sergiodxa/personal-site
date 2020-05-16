7ec12a7c9612d00918dab2967921e1d8aa534e61
Sergio Xalambrí <hello@sergiodxa.com>
2020-05-16 03:38:13 -0500

    Description supports disabling the border

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -1,7 +1,15 @@
-export default function Description({ children }: { children: React.ReactNode }) {
-  return (
-    <p className="text-md md:text-lg tracking-wide border-l-4 border-yellow-500 light:border-indigo-500 pl-4 -ml-4 leading-none">
-      {children}
-    </p>
+import clsx from "clsx";
+
+export default function Description({
+  children,
+  border = true,
+}: {
+  children: React.ReactNode;
+  border?: boolean;
+}) {
+  const cn = clsx(
+    "text-md md:text-lg tracking-wide border-yellow-500 light:border-indigo-500 pl-4 -ml-4 leading-none",
+    { "border-l-4": border }
   );
+  return <p className={cn}>{children}</p>;
 }
