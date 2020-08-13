9f1f505b79dae89517355c8b5b27739618b668a5
Sergio Xalambrí <hello@sergiodxa.com>
2020-08-13 08:23:29 -0500

    Activate fallback and revalidation

--- src/pages/articles/[...slug].tsx
+++ src/pages/articles/[...slug].tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useRouter } from "next/router";
 import { getArticle } from "utils/get-article";
 import { Article } from "types/article";
 import { getListOfArticles } from "utils/get-list-of-articles";
@@ -16,10 +17,13 @@ type Props = {
   content: string;
 };
 
-export async function getStaticProps({ params }): Promise<{ props: Props }> {
+export async function getStaticProps({
+  params,
+}): Promise<{ props: Props; revalidate: number }> {
   const { slug } = params;
   const [meta, content] = await getArticle(slug.join("/"));
   return {
+    revalidate: 1,
     props: {
       slug: slug.join("/"),
       meta,
@@ -40,10 +44,11 @@ export async function getStaticPaths() {
       params: { slug: article.slug.slice("/articles/".length).split("/") },
     };
   });
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }
 
 export default function ArticlePage({ meta, content }: Props) {
+  const { isFallback } = useRouter();
   return (
     <>
       <Header>
@@ -51,24 +56,29 @@ export default function ArticlePage({ meta, content }: Props) {
           <Navigation current="articles" title="Articles" />
 
           <section className="flex items-start mb-4 space-x-4">
-            <div className="mt-4 space-y-2">
-              <h1
-                className="text-xl leading-normal font-semibold"
-                style={{
-                  background: "linear-gradient(45deg, #ED8936, #F6E05E)",
-                  backgroundClip: "text",
-                  WebkitBackgroundClip: "text",
-                  WebkitTextFillColor: "transparent",
-                }}
-              >
-                {meta.title}
-              </h1>
-              {meta.description ? (
-                <p className="leading-7 border-l-4 border-orange-500 pl-2 py-1">
-                  {meta.description}
-                </p>
-              ) : null}
-            </div>
+            {isFallback ? (
+              <TitleSkeleton />
+            ) : (
+              <div className="mt-4 space-y-2">
+                <h1
+                  className="text-xl leading-normal font-semibold"
+                  style={{
+                    background: "linear-gradient(45deg, #ED8936, #F6E05E)",
+                    backgroundClip: "text",
+                    WebkitBackgroundClip: "text",
+                    WebkitTextFillColor: "transparent",
+                  }}
+                >
+                  {meta.title}
+                </h1>
+
+                {meta?.description ? (
+                  <p className="leading-7 border-l-4 border-orange-500 pl-2 py-1">
+                    {meta.description}
+                  </p>
+                ) : null}
+              </div>
+            )}
 
             <Spacer />
 
@@ -83,29 +93,64 @@ export default function ArticlePage({ meta, content }: Props) {
 
       <Container>
         <section className="space-y-2 -mt-6 md:-mt-12 bg-white p-4 rounded-lg border-black border-4 relative mb-6">
-          <article
-            className="prose prose-sm md:prose lg:prose-lg"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
+          {isFallback ? (
+            <ContentSkeleton />
+          ) : (
+            <article
+              className="prose prose-sm md:prose lg:prose-lg"
+              dangerouslySetInnerHTML={{
+                __html: content,
+              }}
+            />
+          )}
         </section>
       </Container>
 
-      <Container>
-        <footer className="mb-12">
-          {meta.cn ? (
-            <p className="text-gray-600 text-sm text-right">
-              Powered by{" "}
-              <a
-                href="https://collectednotes.com"
-                className="underline"
-                rel="noopener noreferrer"
-              >
-                Collected Notes
-              </a>
-            </p>
-          ) : null}
-        </footer>
-      </Container>
+      {!isFallback ? (
+        <Container>
+          <footer className="mb-12">
+            {meta.cn ? (
+              <p className="text-gray-600 text-sm text-right">
+                Powered by{" "}
+                <a
+                  href="https://collectednotes.com"
+                  className="underline"
+                  rel="noopener noreferrer"
+                >
+                  Collected Notes
+                </a>
+              </p>
+            ) : null}
+          </footer>
+        </Container>
+      ) : null}
     </>
+  );
+}
+
+function TitleSkeleton() {
+  return (
+    <div
+      className="animate-pulse h-5 bg-gray-400 rounded w-3/5"
+      style={{
+        marginTop: 32,
+      }}
+    />
+  );
+}
+
+function ContentSkeleton() {
+  return (
+    <article className="space-y-4 animate-pulse">
+      <div className="h-4 bg-gray-400 rounded w-2/3" />
+      <div className="h-4 bg-gray-400 rounded w-4/5" />
+      <div className="h-4 bg-gray-400 rounded w-2/3" />
+      <div className="h-4 bg-gray-400 rounded" />
+      <div className="h-4 bg-gray-400 rounded w-3/5" />
+      <div className="h-4 bg-gray-400 rounded w-1/3" />
+      <div className="h-4 bg-gray-400 rounded w-4/5" />
+      <div className="h-4 bg-gray-400 rounded" />
+      <div className="h-4 bg-gray-400 rounded w-3/5" />
+    </article>
   );
 }
