675f582056d18608a2752f87c279ff35372ea094
Sergio Xalambrí <hello@sergiodxa.com>
2020-08-13 18:43:31 -0500

    Send empty response

--- src/pages/api/webhooks/cn.ts
+++ src/pages/api/webhooks/cn.ts
@@ -1,6 +1,7 @@
-import { NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import { resolve } from "url";
 import { Note } from "collected-notes";
+import { RSA_NO_PADDING } from "constants";
 
 type NoteUpdatedEvent = { type: "note-updated"; data: { note: Note } };
 type NoteCreatedEvent = { type: "note-created"; data: { note: Note } };
@@ -8,9 +9,13 @@ type NoteEvent = NoteUpdatedEvent | NoteCreatedEvent;
 
 const URL = process.env.VERCEL_URL || "localhost:3000";
 
-export default function webhookCollectedNotes(req: NextApiRequest) {
+export default function webhookCollectedNotes(
+  req: NextApiRequest,
+  res: NextApiResponse<"">
+) {
   const event = req.body as NoteEvent;
   if (event.type === "note-updated" || "note-created") {
-    return fetch(resolve(URL, `/articles/${event.data.note.path}`));
+    fetch(resolve(URL, `/articles/${event.data.note.path}`));
   }
+  res.send("");
 }
