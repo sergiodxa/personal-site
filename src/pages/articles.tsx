d154b8d86e4141dc81db18a7facc294e52db4540
Sergio Xalambrí <hello@sergiodxa.com>
2020-08-23 00:32:34 -0500

    Remove unused import

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { GetStaticPropsResult } from "next";
-import Router, { useRouter } from "next/router";
 import Link from "next/link";
 import matchSorter from "match-sorter";
 import { Navigation } from "components/navigation";
