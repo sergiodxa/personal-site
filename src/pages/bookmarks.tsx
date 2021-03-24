2ca2f82254d08e45e2600f3b1acd6ec0be770873
Sergio Xalambrí <hello@sergiodxa.com>
2021-03-24 17:54:31 -0500

    Update bookmarks.tsx

--- src/pages/bookmarks.tsx
+++ src/pages/bookmarks.tsx
@@ -5,7 +5,7 @@ import { BookmarksLayout } from "layouts/bookmarks";
 
 export const getStaticProps: GetStaticProps<BookmarksPageProps> = async () => {
   const bookmarks = await getBookmarks();
-  return { props: { bookmarks }, revalidate: 5 };
+  return { props: { bookmarks }, revalidate: 60 };
 };
 
 export default BookmarksLayout;
