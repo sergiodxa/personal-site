3fe8f4d161b8073ef921db398883d6d00505cae8
Sergio Xalambrí <hello@sergiodxa.com>
2021-11-25 18:45:25 -0500

    Update session.server.ts

--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,15 +1,16 @@
 import { createCookieSessionStorage } from "remix";
 import { env } from "~/utils";
 
-export let { getSession, commitSession, destroySession } =
-  createCookieSessionStorage({
-    cookie: {
-      name: "__web_session",
-      sameSite: "lax",
-      path: "/",
-      httpOnly: true,
-      secrets: ["s3cr3t"],
-      secure: env("production"),
-      domain: env("production") ? "sergiodxa.com" : undefined,
-    },
-  });
+export let sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__web_session",
+    sameSite: "lax",
+    path: "/",
+    httpOnly: true,
+    secrets: ["s3cr3t"],
+    secure: env("production"),
+    domain: env("production") ? "sergiodxa.com" : undefined,
+  },
+});
+
+export let { getSession, commitSession, destroySession } = sessionStorage;
